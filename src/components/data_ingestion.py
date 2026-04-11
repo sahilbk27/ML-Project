c05a18d17728081540d231ef9f70c7b5059130f2
Sahil_Khot <khotsahil27@gmail.com>
2026-04-11 15:23:45 +0530

    update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,10 +9,10 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('arifacts',"train.csv")
-    test_data_path: str=os.path.join('arifacts',"test.csv")
-    raw_data_path: str=os.path.join('arifacts',"data.csv")
-
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+ 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
